d4185838d2e7cb7f34cadf2067c15acca22ee952
Eunjung Jenny Jeon <jenny.it.kr@gmail.com>
2020-02-12 11:38:56 +0900

    Solve 2477

--- 2477_참외밭.py
+++ 2477_참외밭.py
@@ -3,24 +3,43 @@ N = int(input())
 elem = [list(map(int, input().split())) for _ in range(6)]
 w = [l for d, l in elem if d == 2 or d == 1]
 h = [l for d, l in elem if d == 3 or d == 4]
-elem_len = [l for d, l in elem]
 
-max_w = max(w)
-max_h = max(h)
+max_w = 0
+max_w_idx = 0
+max_h = 0
+max_h_idx = 0
+
+for idx, (d, l) in enumerate(elem):
+    if d in (1, 2) and l > max_w:
+        max_w = l
+        max_w_idx = idx    
+    elif d in (3, 4) and l > max_h:
+        max_h = l
+        max_h_idx = idx    
+
 tot_area = max_w * max_h
 
-max_w_idx = elem_len.index(max_w)
-max_h_idx = elem_len.index(max_h)
+find_idx = [-1, 1]
 
-cut_idx1 = max(max_w_idx, max_h_idx) + 2
-cut_idx2 = max(max_w_idx, max_h_idx) + 3
+for i in range(2):
+    check_idx = max_h_idx + find_idx[i]
+    if check_idx > 5: 
+        check_idx %= 6
+    check_w = elem[check_idx][1]
+    if check_w != max_w:
+        cut_w = max_w - check_w
+        break
 
-if cut_idx1 >= 6:
-    cut_idx1 %= 6
-if cut_idx2 >= 6:
-    cut_idx2 %= 6
+for i in range(2):
+    check_idx = max_w_idx + find_idx[i]
+    if check_idx > 5: 
+        check_idx %= 6
+    check_h = elem[check_idx][1]
+    if check_h != max_h:
+        cut_h = max_h - check_h
+        break    
 
-cut_area = elem_len[cut_idx1] * elem_len[cut_idx2]
+cut_area = cut_w * cut_h
 
 result = N * (tot_area - cut_area)
 print(result)
